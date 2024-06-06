77da219a623c28b90832fa44427f07406974dce7
Prajwal-PL <prajwalpl096@gmail.com>
2024-06-06 23:13:26 +0530

    Added queries logic, server actions and authentication optimisation

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,6 +1,12 @@
+import { getAuthDetails, verifyAndAcceptInvitaitions } from "@/lib/queries";
 import React from "react";
 
-const Agency = () => {
+const Agency = async () => {
+  const agencyId = await verifyAndAcceptInvitaitions();
+  console.log(agencyId);
+
+  const user = await getAuthDetails();
+
   return <div>Agency Dashboard</div>;
 };
 
